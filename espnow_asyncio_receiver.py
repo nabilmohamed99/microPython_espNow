a5ac3c921f0e561f077823a28231b7c2de8a60b6
Mohammed Nabil <63048936+nabilmohamed99@users.noreply.github.com>
2024-03-20 10:35:25 +0000

    Update espnow_asyncio_receiver.py
    correction, du code de receiver

--- espnow_asyncio_receiver.py
+++ espnow_asyncio_receiver.py
@@ -1,15 +1,13 @@
 import network
 import aioespnow
-from machine import Pin,PWM
 import asyncio
 
-# A WLAN interface must be active to send()/recv()
 sta = network.WLAN(network.STA_IF)  # Or network.AP_IF
 sta.active(True)
 sta.disconnect()      # For ESP8266
 
-# Initialize ESP-NOW
-esp = aioespnow.AIOESPNow()  # Returns AIOESPNow enhanced with async support
+
+esp = aioespnow.AIOESPNow()  
 esp.active(True)
 
 
@@ -28,9 +26,7 @@ async def wait_for_message():
                 string_data = msg.decode('utf-8')
                 potentiometer_value = string_data.split(":")[1]
                 print(f"Pot Value: {potentiometer_value}")
-                # Map the potentiometer value to PWM duty cycle (0 to 1023)
-                #duty_cycle = int(map_value(int(potentiometer_value), 0, 4095, 0, 1023))
-               # pwm.duty(duty_cycle)
+               
             else:
                 print(f"Unknown message {msg}")
             
